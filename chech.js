324903778a037c9423502ce3cdc6bda21ddc3814
DanFosv2 <138312355+DanFosv2@users.noreply.github.com>
2023-07-07 17:10:55 +0100

    Update chech.js
    adding a logout function

--- chech.js
+++ chech.js
@@ -3,3 +3,10 @@ if(window.sessionStorage.logedin != "yes"){
   window.location.href = "login.html";
 }
 
+function logout(){
+  window.sessionStorage.logedin = "no";
+  if(window.sessionStorage.logedin != "yes"){
+    alert("You are not logged in please log in")
+    window.location.href = "login.html";
+  }
+}
